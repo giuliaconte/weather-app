9c28988946a3d8303880d6ed2088fbf2d9518e99
Giulia <giuliac@outlook.com>
2023-01-27 10:23:29 +0200

    third commit finalized

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ const iconImg = document.getElementById('weather-icon'),
     tempF = document.querySelector('.f'),
     desc = document.querySelector('.desc'),
     sunriseDOM = document.querySelector('.sunrise'),
-    sunsetDOM = document.querySelector('.sunset')
+    sunsetDOM = document.querySelector('.sunset');
 
 // event listener that fires up when page loads and executes the function
 window.addEventListener('load', () => {
@@ -23,35 +23,40 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude
 
 // JS fetch API, JS promise
-            const base = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric'
+            const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`
             fetch(base)
             .then((response) => {
-            return response.json();
+            console.log(response)
+                return response.json();
         })
 
 // JSON returns the response to an object. destructuring:
         .then ((data) => {
+            console.log(data.main)
             const {temp} = data.main,
                 place = data.name,
                 {description, icon} = data.weather[0],
                 {sunrise, sunset} = data.sys;
-
-            const iconUrl = ' http://openweathermap.org/img/wn/${icon}@2x.png'
+            console.log(icon)
+            const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
             const fahrenheit = (temp * 9) / 5 + 32
 
 // converting epoch time to GMT
             const sunriseGMT = new Date(sunrise * 1000)
             const sunsetGMT = new Date(sunset * 1000)
+
+            // interacting with DOM to show the data. if defined outside of the "then", they would be undefined!!!
+            // template literals
+            loc.textContent = `${place}`
+            desc.textContent = `${description}`
+            tempC.textContent = `${temp.toFixed(2)} °C`
+            tempF.textContent = `${fahrenheit.toFixed(2)} °F`
+            sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`
+            sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`
+
+            // change src tag og the image with .src
+            iconImg.src = iconUrl
         })
+    })  
     }
 })
-
-// change src tag og the image
-iconImg = iconUrl
-
-// template literals
-loc.textContent = `${place}`
-desc.textContent = `${description}`
-tempC.textContent = `${temp.toFixed(2)} °C`
-tempF.textContent = `${fahrenheit.toFixed(2)} °F`
-sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}`
